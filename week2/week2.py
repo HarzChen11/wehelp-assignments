2b2fcc689983e37c97007aa2d3c53127ae07b176
Harz <77651113+HarzChen11@users.noreply.github.com>
2022-01-06 19:58:31 +0800

    Update week2.py

--- week2/week2.py
+++ week2/week2.py
@@ -71,10 +71,3 @@ result=twoSum([2, 11, 7, 15], 9)
 print(result) # show [0, 2] because nums[0]+nums[2] is 9
 
 
-
-
-# maxProduct([5, 20, 2, 6])  # 得到 120
-# maxProduct([10, -20, 0, 3])  # 得到 30
-# maxProduct([-1, 2])  # 得到 -2
-# maxProduct([-1, 0, 2])  # 得到 0
-# maxProduct([-1, -2, 0])  # 得到 2
